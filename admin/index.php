39e5983fc1e6255643f990023034e9cb1ea134f5
nahHrnndz <hernandezmahanna6@gmail.com>
2022-10-25 18:48:26 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -29,23 +29,6 @@
           </div>
         </section>
 
-        <!-- /.content -->
-  <div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
   <div class="modal fade" id="uni_modal" role='dialog'>
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
       <div class="modal-content">
