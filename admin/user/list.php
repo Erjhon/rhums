309e91c6e60cb34389d4161e6399485f7802bc28
nahHrnndz <hernandezmahanna6@gmail.com>
2022-10-25 18:58:10 +0800

    Update list.php

--- admin/user/list.php
+++ admin/user/list.php
@@ -74,7 +74,7 @@
         <div id="delete_content"></div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continuefd</button>
+        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
       </div>
