2c9179b7831115f0ec0f55140c617802e6f386ec
doffie-JC <johdigay@my.cspc.edu.ph>
2022-11-05 14:26:07 +0800

    sasdasd

--- classes/Login.php
+++ classes/Login.php
@@ -15,6 +15,8 @@ class Login extends DBConnection {
 	public function index(){
 		echo "<h1>Access Denied</h1> <a href='".base_url."'>Go Back.</a>";
 	}
+
+
 	public function login(){
 		extract($_POST);
 
@@ -32,14 +34,16 @@ class Login extends DBConnection {
 		}elseif($qry2->num_rows > 0){
 		    return json_encode(array('status'=>'success', 'is_admin' => 0));
 		}else{
-		return json_encode(array('status'=>'incorrect','last_qry'=>"SELECT * from users where username = '$username' and password = md5('$password') "));
+			return json_encode(array('status'=>'incorrect','last_qry'=>"SELECT * from users where username = '$username' and password = md5('$password') "));
 		}
 	}
+
 	public function logout(){
 		if($this->settings->sess_des()){
 			redirect('admin/login.php');
 		}
 	}
+
 	function login_user(){
 		extract($_POST);
 		$qry = $this->conn->query("SELECT * from users where usersname = '$usersname' and password = md5('$password') ");
@@ -58,7 +62,27 @@ class Login extends DBConnection {
 		}
 		return json_encode($resp);
 	}
+
+	function login_for_all(){
+		extract($_POST);
+
+		if(strtolower($username) == 'admin'){
+			$admin = $this->conn->query("SELECT * from users where username = '$username' and password = md5('$password') ");
+			if($qry->num_rows > 0){
+				foreach($qry->fetch_array() as $k => $v){
+					$this->settings->set_userdata($k,$v);
+				}
+				$this->settings->set_userdata('login_type',1);
+
+				return json_encode([
+					
+				]);
+			}
+		}
+
+	}
 }
+
 $action = !isset($_GET['f']) ? 'none' : strtolower($_GET['f']);
 $auth = new Login();
 switch ($action) {
