343ede722dbaf9396ffcb068d4fa7327232530e5
nahHrnndz <hernandezmahanna6@gmail.com>
2022-11-06 20:56:31 +0800

    Update list.php

--- admin/user/list.php
+++ admin/user/list.php
@@ -52,8 +52,8 @@
 				                  </button>
 				                  <div class="dropdown-menu" role="menu">
 				                    <a class="dropdown-item" href="?page=user/manage_user&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+				                    <!-- <div class="dropdown-divider"></div> -->
+				                    <!-- <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a> -->
 				                  </div>
 							</td>
 						</tr>
