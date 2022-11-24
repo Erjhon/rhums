1d00d60e31c7bcfe77497f1afd05ba000cd38622
mscrizel <domagsangcrizelb13@gmail.com>
2022-11-24 14:39:01 +0800

    responsive patient dashbrd

--- inc/header.php
+++ inc/header.php
@@ -27,9 +27,9 @@
     <!-- JQVMap -->
     <link rel="stylesheet" href="<?php echo base_url ?>plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-  <!--   <link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css">
+   <!-- <link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css"> -->
     <link rel="stylesheet" href="<?php echo base_url ?>dist/css/custom.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>assets/css/styles.css"> -->
+    <!-- <link rel="stylesheet" href="<?php echo base_url ?>assets/css/styles.css">  -->
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="<?php echo base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
