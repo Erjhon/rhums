c35c9a28e3d1e1f98341965abe4cdf851796e27b
nahHrnndz <hernandezmahanna6@gmail.com>
2022-12-22 23:47:42 +0800

    privacy policy

--- patient/register.php
+++ patient/register.php
@@ -105,6 +105,9 @@ if(isset($_POST['submit'])){
             color: red;
             font-size: 13px;
           }
+          p {
+            text-align: justify;
+          }
         </style>
       </head>
 
@@ -124,7 +127,6 @@ if(isset($_POST['submit'])){
               <div class="header-body text-center mb-5">
                 <div class="row justify-content-center">
                   <div class="col-lg-8 col-md-1"><br><br>
-                    <!-- <h1 class="text-white">RURAL HEALTH UNIT II</h1> -->
                   </div>
                 </div>
               </div>
@@ -236,50 +238,50 @@ if(isset($_POST['submit'])){
         <h5 class="text-dark required">Address</h5>
         <!-- <input class="form-control" placeholder="Address" id="address" name="address" type="address"> -->
          <select class="form-control" id="address" value="<?php echo isset($_POST['address']) ? $_POST['address'] : ''; ?>" name="address" rows="2" onchange ="return validate('address')" >
-                                                    <option class="placeholder" style="display: none" value="">Select Address</option>
-                                                  <option>Angustia, Nabua</option>
-                                                  <option>Antipolo Old, Nabua</option>
-                                                  <option>Antipolo Young, Nabua</option>
-                                                  <option>Aro-aldao, Nabua</option>
-                                                  <option>Bustrac, Nabua</option>
-                                                  <option>Dolorosa, Nabua</option>
-                                                  <option>Duran, Nabua</option>
-                                                  <option>Inapatan, Nabua</option>
-                                                  <option>La Opinion, Nabua</option>
-                                                  <option>La Purisima, Nabua</option>
-                                                  <option>Lourdes Old, Nabua</option>
-                                                  <option>Lourdes Young, Nabua</option>
-                                                  <option>Malawag, Nabua</option>
-                                                  <option>Paloyon Oriental, Nabua</option>
-                                                  <option>Paloyon Proper, Nabua</option>
-                                                  <option>Salvacion Que Gatos, Nabua</option>
-                                                  <option>San Antonio, Nabua</option>
-                                                  <option>San Antonio Ogbon, Nabua</option>
-                                                  <option>San Esteban, Nabua</option>
-                                                  <option>San Francisco, Nabua</option>
-                                                  <option>San Isidro, Nabua</option>
-                                                  <option>San Isidro Inapatan, Nabua</option>
-                                                  <option>San Jose, Nabua</option>
-                                                  <option>San Juan, Nabua</option>
-                                                  <option>San Luis, Nabua</option>
-                                                  <option>San Miguel, Nabua</option>
-                                                  <option>San Nicolas, Nabua</option>
-                                                  <option>San Roque, Nabua</option>
-                                                  <option>San Roque Madawon, Nabua</option>
-                                                  <option>San Roque Sagumay, Nabua</option>
-                                                  <option>San Vicente Gorong-Gorong, Nabua</option>
-                                                  <option>San Vicente Ogbon, Nabua</option>
-                                                  <option>Santa Barbara, Nabua</option>
-                                                  <option>Santa Cruz, Nabua</option>
-                                                  <option>Santa Elena Baras, Nabua</option>
-                                                  <option>Santa Lucia Baras, Nabua</option>
-                                                  <option>Santiago Old, Nabua</option>
-                                                  <option>Santiago Young, </option>
-                                                  <option>Santo Domingo, Nabua</option>
-                                                  <option>Tandaay, Nabua</option>
-                                                  <option>Topas Proper, Nabua</option>
-                                                  <option>Topas Sogod, Nabua</option>
-                                                    </select>
+            <option class="placeholder" style="display: none" value="">Select Address</option>
+            <option>Angustia, Nabua</option>
+            <option>Antipolo Old, Nabua</option>
+            <option>Antipolo Young, Nabua</option>
+            <option>Aro-aldao, Nabua</option>
+            <option>Bustrac, Nabua</option>
+            <option>Dolorosa, Nabua</option>
+            <option>Duran, Nabua</option>
+            <option>Inapatan, Nabua</option>
+            <option>La Opinion, Nabua</option>
+            <option>La Purisima, Nabua</option>
+            <option>Lourdes Old, Nabua</option>
+            <option>Lourdes Young, Nabua</option>
+            <option>Malawag, Nabua</option>
+            <option>Paloyon Oriental, Nabua</option>
+            <option>Paloyon Proper, Nabua</option>
+            <option>Salvacion Que Gatos, Nabua</option>
+            <option>San Antonio, Nabua</option>
+            <option>San Antonio Ogbon, Nabua</option>
+            <option>San Esteban, Nabua</option>
+            <option>San Francisco, Nabua</option>
+            <option>San Isidro, Nabua</option>
+            <option>San Isidro Inapatan, Nabua</option>
+            <option>San Jose, Nabua</option>
+            <option>San Juan, Nabua</option>
+            <option>San Luis, Nabua</option>
+            <option>San Miguel, Nabua</option>
+            <option>San Nicolas, Nabua</option>
+            <option>San Roque, Nabua</option>
+            <option>San Roque Madawon, Nabua</option>
+            <option>San Roque Sagumay, Nabua</option>
+            <option>San Vicente Gorong-Gorong, Nabua</option>
+            <option>San Vicente Ogbon, Nabua</option>
+            <option>Santa Barbara, Nabua</option>
+            <option>Santa Cruz, Nabua</option>
+            <option>Santa Elena Baras, Nabua</option>
+            <option>Santa Lucia Baras, Nabua</option>
+            <option>Santiago Old, Nabua</option>
+            <option>Santiago Young, </option>
+            <option>Santo Domingo, Nabua</option>
+            <option>Tandaay, Nabua</option>
+            <option>Topas Proper, Nabua</option>
+            <option>Topas Sogod, Nabua</option>
+          </select>
          <p  class="text-danger" id="ad" style="font-size:12px;"></p>
       </div>
 
@@ -322,7 +324,9 @@ if(isset($_POST['submit'])){
           <div class="custom-control custom-control-alternative custom-checkbox text-center">
             <input class="custom-control-input" id="customCheckRegister" type="checkbox" required>
             <label class="custom-control-label" for="customCheckRegister">
+              <!-- <span class="text-dark">I agree with the <a href="privacy.php" target="_blank">Privacy Policy</a></span> -->
               <span class="text-dark">I agree with the <a href="https://www.privacypolicygenerator.info/live.php?token=W9MHivbFeduOOsIHHIsk5jbZ2d92trv9"  data-toggle="modal" data-target="#exampleModal">Privacy Policy</a></span>
+
             </label>
           </div>
         </div>
@@ -342,6 +346,7 @@ if(isset($_POST['submit'])){
 </div>
 </div>
 
+
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
@@ -353,95 +358,62 @@ if(isset($_POST['submit'])){
         </button>
       </div>
       <div class="modal-body">
-       <h1>Privacy Policy for RURAL HEALTH UNIT II</h1>
-
+        <h1>Privacy Policy for Rural Health Unit II</h1>
+<!-- 
 <p>At  Medical Appointment and Record Management System, accessible from http://ruralhealthunit2stodomingo.epizy.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by  Medical Appointment and Record Management System and how we use it.</p>
 
 <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
 
 <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in  Medical Appointment and Record Management System. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info/">Free Privacy Policy Generator</a>.</p>
+ -->
 
-<h2>Consent</h2>
+        <p>At Rural Health Unit II, one of our main priorities is the privacy of our visitors. This privacy policy document contains types of information that is collected and recorded by Rural Health Unit II and how we use it.</p>
 
-<p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+        <h2>Consent</h2>
 
-<h2>Information we collect</h2>
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
 
-<p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
-<p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
-<p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
+        <h2>Information we collect</h2>
 
-<h2>How we use your information</h2>
+        <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
+        <p>When you register for an account, we may ask for your contact information, including items such as name, username, address, email address, and contact number, gender, date of birth, address, password and picture.</p>
+        <p>Medical History will also be collected.</p>
+        <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
 
-<p>We use the information we collect in various ways, including to:</p>
+        <h2>How we use your information</h2>
 
-<ul>
-<li>Provide, operate, and maintain our website</li>
-<li>Improve, personalize, and expand our website</li>
-<li>Understand and analyze how you use our website</li>
-<li>Develop new products, services, features, and functionality</li>
-<li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
-<li>Send you emails</li>
-<li>Find and prevent fraud</li>
-</ul>
+        <p>We use the information we collect in various ways, including to:</p>
 
-<h2>Log Files</h2>
+        <ul>
+          <li>Provide, operate, and maintain our website.</li>
+          <li>Communicate with you to provide you with updates and other information relating to the website.</li>
+          <li>Send you emails.</li>
+        </ul>
 
-<p> Medical Appointment and Record Management System follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+        <h2>Data Protection Rights</h2>
 
-<h2>Cookies and Web Beacons</h2>
+        <p>Rural Health Unit II would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+        <p>The right to access – You have the right to request copies of your personal data.<br>
+        The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.<br>
+        The right to erasure – You have the right to request that we erase your personal data, under certain conditions.<br>
+        The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.<br>
+        The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
 
-<p>Like any other website,  Medical Appointment and Record Management System uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+        <h2>Children's Information</h2>
 
+        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
+        <p>Rural Health Unit II does not knowingly collect any Personal Identifiable Information from children under the age of 5. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
 
-<h2>Advertising Partners Privacy Policies</h2>
-
-<P>You may consult this list to find the Privacy Policy for each of the advertising partners of  Medical Appointment and Record Management System.</p>
-
-<p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on  Medical Appointment and Record Management System, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-
-<p>Note that  Medical Appointment and Record Management System has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-<h2>Third Party Privacy Policies</h2>
-
-<p> Medical Appointment and Record Management System's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
-
-<p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
-
-<h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
-
-<p>Under the CCPA, among other rights, California consumers have the right to:</p>
-<p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
-<p>Request that a business delete any personal data about the consumer that a business has collected.</p>
-<p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
-<p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
-
-<h2>GDPR Data Protection Rights</h2>
-
-<p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
-<p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p>
-<p>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
-<p>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p>
-<p>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p>
-<p>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
-<p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
-<p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
-
-<h2>Children's Information</h2>
-
-<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
-
-<p> Medical Appointment and Record Management System does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
+        <p>If you have any concerns, you may contact Rural Health Unit II at <a href="mailto:nabua.rhu2@gmail.com">nabua.rhu2@gmail.com.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
 </div>
-
+</div>
 
 
 <!-- Checks Form validation -->
