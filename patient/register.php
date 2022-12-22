4108ff5c2c26a4b444998d76a8d45a0616924999
Erjhon Baldoza <erjhonbaldoza@gmail.com>
2022-12-23 00:06:07 +0800

    modal-lg

--- patient/register.php
+++ patient/register.php
@@ -349,7 +349,7 @@ if(isset($_POST['submit'])){
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Privacy Policy</h5>
